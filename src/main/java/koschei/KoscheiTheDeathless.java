4dfb86df80fe3705ae268853c1070c58f3770d0e
Muslim217 <abumuslimsafaraliev3815@gmail.com>
2023-06-11 16:56:58 +0300

    Version1

--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -2,9 +2,11 @@ package koschei;
 
 import koschei.models.Ocean1;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
+
 public class KoscheiTheDeathless {
 
     private Ocean1 ocean;
